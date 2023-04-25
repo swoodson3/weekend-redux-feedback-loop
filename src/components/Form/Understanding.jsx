37f9b9d30de2401cd58c6478a80eed7d1a8808aa
Seth Woodson <sethwoodson3@gmail.com>
2023-04-25 16:16:23 -0500

    fixed validation on understanding component and added style

--- src/components/Form/Understanding.jsx
+++ src/components/Form/Understanding.jsx
@@ -14,24 +14,40 @@ function UnderstandingOne() {
             dispatch (action);
         } else {
             alert('Please enter a number between 1 to 5.');
+            event.target.value = '';
         }
         
     }
  
     const nextPage = () => {
-        if (understandingOne > 0 && understandingOne < 6) {
-            history.push('/support');
+        if (!isNaN(understandingOne) && understandingOne >= 1 && understandingOne <= 5) {
+          history.push('/support');
         } else {
+          alert('Please enter a number between 1 to 5.');
         }
-    }
+      }
 
 
     return (
         <>
-            <h3>How well are you understanding the content on a scale of 1 to 5?</h3>
-                <input onChange={handleChange} type="number" placeholder='Leave a rating' required />
-                <input type="submit" />
-                <button onClick={nextPage}>Next</button>
+            <h3 style= {{fontSize: 30 }}>How well are you understanding the content on a scale of 1 to 5?</h3>
+                <input className ="inputButtontwo" style={{width: '150px', height: '20px', borderRadius: '5px', border: '1px solid gray', padding: '5px' }} onChange={handleChange} type="number" placeholder='Leave a rating' />
+                <input className="submitButtontwo"
+                style={{
+                    backgroundColor: 'navy',
+                    color: 'white',
+                    frontWeight: 'bold',
+                    width: '60px',
+                    height: '30px'
+                }} type="submit" />
+                <button className="nextPageButtontwo"
+                style={{
+                    backgroundColor: 'navy',
+                    color: 'white',
+                    frontWeight: 'bold',
+                    width: '60px',
+                    height: '30px'
+                }} onClick={nextPage}>Next</button>
         </>
     )
 }
